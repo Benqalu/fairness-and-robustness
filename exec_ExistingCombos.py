49fe4bee02d4d4d708df62bb2fe2197463b457bc
Benqalu <43207684+Benqalu@users.noreply.github.com>
2021-03-12 18:06:48 -0500

    1

--- exec_ExistingCombos.py
+++ exec_ExistingCombos.py
@@ -8,13 +8,14 @@ prngs={
 }
 
 try:
-	for func in ['fairness_reweighing', 'fairness_disparate', 'fairness_adversarial']:
-		for method in ['FGSM','PGD']:
-			for data in ['compas','adult']:
-				for attr in ['race','sex']:
-					for pidx in range(prngs[func]):
-						# print(func, method, data, attr, pidx)
-						os.system('python ExistingCombos.py %s %s %s %s %s'%(func, method, data, attr, pidx))
-						sleep(3)
+	for _ in range(100):
+		for func in ['fairness_reweighing', 'fairness_disparate', 'fairness_adversarial']:
+			for method in ['FGSM','PGD']:
+				for data in ['compas','adult']:
+					for attr in ['race','sex']:
+						for pidx in range(prngs[func]):
+							# print(func, method, data, attr, pidx)
+							os.system('python ExistingCombos.py %s %s %s %s %s'%(func, method, data, attr, pidx))
+							sleep(3)
 except KeyboardInterrupt:
 	pass
