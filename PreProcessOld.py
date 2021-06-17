b2e8920dd00a11ed4f63aa4993f28c91c2b0f935
Benqalu <43207684+Benqalu@users.noreply.github.com>
2021-06-17 00:22:43 -0400

    1

--- PreProcessOld.py
+++ PreProcessOld.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+
 np.set_printoptions(suppress=False)
 from torch.autograd import grad as gradient
 from pandas import DataFrame
@@ -9,10 +10,13 @@ from TorchAdversarial import TorchNNCore
 from utils import load_split
 from metric import Metric
 
+
 class PreProcFlip(object):
-	def __init__(self, data, attr, method='FGSM', k=10, seed=None, max_iter=1000, dR=0.1, dF=0.1):
+	def __init__(
+		self, data, attr, method="FGSM", k=10, seed=None, max_iter=1000, dR=0.1, dF=0.1
+	):
 		print((data, attr, method, dF, dR))
-		
+
 		self._data = data
 		self._attr = attr
 		self._seed = seed
@@ -30,31 +34,37 @@ class PreProcFlip(object):
 		if type(k) is int:
 			self._k = k
 		elif type(k) is float:
-			self._k = np.ceil(self._train_np['X'].shape[0] * k).astype(int)
+			self._k = np.ceil(self._train_np["X"].shape[0] * k).astype(int)
 		else:
-			raise ValueError('k must be float or int')
+			raise ValueError("k must be float or int")
 
-		print('Flipping %.4f%% (%d) records every epoch'%(self._k*100.0/self._train_np['X'].shape[0], self._k))
+		print(
+			"Flipping %.4f%% (%d) records every epoch"
+			% (self._k * 100.0 / self._train_np["X"].shape[0], self._k)
+		)
 
 		self._train = {
-			'X':torch.tensor(self._train_np['X'], dtype=torch.float),
-			'y':torch.tensor(self._train_np['y'].reshape(-1,1), dtype=torch.float),
-			's':self._train_np['s']
-		}
-		
-		self._test = {
-			'X':torch.tensor(self._test_np['X'], dtype=torch.float),
-			'y':torch.tensor(self._test_np['y'].reshape(-1,1), dtype=torch.float),
+			"X": torch.tensor(self._train_np["X"], dtype=torch.float),
+			"y": torch.tensor(self._train_np["y"].reshape(-1, 1), dtype=torch.float),
+			"s": self._train_np["s"],
 		}
 
+		self._test = {
+			"X": torch.tensor(self._test_np["X"], dtype=torch.float),
+			"y": torch.tensor(self._test_np["y"].reshape(-1, 1), dtype=torch.float),
+		}
 
 	def _BCELoss(self, y_pred, y, reduction=True):
-		if reduction:			
+		if reduction:
 			return -torch.mean(
-				y * torch.log(0.99 * y_pred) + (1.0 - y) * torch.log(1.0 - 0.99 * y_pred)
+				y * torch.log(0.99 * y_pred)
+				+ (1.0 - y) * torch.log(1.0 - 0.99 * y_pred)
 			)
 		else:
-			return -(y * torch.log(0.99 * y_pred) + (1.0 - y) * torch.log(1.0 - 0.99 * y_pred))
+			return -(
+				y * torch.log(0.99 * y_pred)
+				+ (1.0 - y) * torch.log(1.0 - 0.99 * y_pred)
+			)
 
 	def _DISPLoss(self, c, y_pred):
 		return torch.square(torch.sum(c * y_pred))
@@ -64,13 +74,13 @@ class PreProcFlip(object):
 		return metric.accuracy(), metric.positive_disparity(s=s)
 
 	def _Scaler(self, a_):
-		a=np.array(a_)
+		a = np.array(a_)
 		a_min = a.min()
 		a_max = a.max()
 		if a_min == a_max:
 			return a
 		else:
-			return (a - a_min)/(a_max - a_min)*2 - 1
+			return (a - a_min) / (a_max - a_min) * 2 - 1
 
 	def AdvExp(self, X, y=None, method="FGSM", eps=0.1):
 		if type(X) is torch.Tensor:
@@ -102,6 +112,8 @@ class PreProcFlip(object):
 
 	def fit_transform(self, test_output=True):
 
+		judgement_set = "train"
+
 		res = {
 			"setting": "%s_%s" % (self._data, self._attr),
 			"train": {
@@ -119,7 +131,7 @@ class PreProcFlip(object):
 		}
 
 		model = TorchNNCore(
-			inps=self._train['X'].shape[1],
+			inps=self._train["X"].shape[1],
 			hiddens=[128],
 			seed=self._seed,
 			hidden_activation=torch.nn.ReLU,
@@ -129,7 +141,7 @@ class PreProcFlip(object):
 		optim = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
 		loss_func = torch.nn.BCELoss()
 
-		self._train['X_adv'] = None
+		self._train["X_adv"] = None
 
 		chosen_R = []
 		chosen_F = []
@@ -137,12 +149,16 @@ class PreProcFlip(object):
 
 		for it in range(0, self._max_iter):
 
-			if self._train['X_adv'] is not None:
-				X_train = torch.vstack([self._train['X'], self._train['X_adv']]).detach()
-				y_train = torch.vstack([self._train['y'], self._train['y'][chosen_R]]).detach()
+			if self._train["X_adv"] is not None:
+				X_train = torch.vstack(
+					[self._train["X"], self._train["X_adv"]]
+				).detach()
+				y_train = torch.vstack(
+					[self._train["y"], self._train["y"][chosen_R]]
+				).detach()
 			else:
-				X_train = self._train['X'].detach()
-				y_train = self._train['y'].detach()
+				X_train = self._train["X"].detach()
+				y_train = self._train["y"].detach()
 
 			# BEGIN: Train modified model
 			tolerence = 10
@@ -164,15 +180,54 @@ class PreProcFlip(object):
 
 			res["iter"].append(it + 1)
 
-			infl = -np.ones(self._train['s'].shape[0])
-			if ((len(res["test"]["disp"])==0) or (len(res["test"]["disp"])>0 and res["test"]["disp"][-1] > self._dF)) and len(chosen_F) < self._train['X'].shape[0]:
-				# BEGIN: Pre-processing of fairness
-				y_pred_np = model(self._train['X']).detach().numpy().reshape(-1)
-				metric = Metric(pred=y_pred_np, true=self._train['y'].detach().numpy().reshape(-1))
-				disp = metric.positive_disparity(s=self._train['s'], absolute=False)
-				infl = np.sign(
-					disp * (self._train['s'] - 0.5) * (self._train_np['y'] - 0.5)
-				) * (0.5 - abs(y_pred_np - 0.5))
+			# BEGIN: Pre-processing of fairness
+			infl = -np.ones(self._train["s"].shape[0])
+			if (
+				len(res[judgement_set]["disp"]) > 0
+				and res[judgement_set]["disp"][-1] > self._dF
+				and len(chosen_F) < self._train["X"].shape[0]
+			):
+				y_pred = model(X_train)
+
+				loss_orig = self._BCELoss(y_pred, y_train, reduction=False)
+				loss_flip = self._BCELoss(y_pred, 1 - y_train, reduction=False)
+
+				I_theta = []
+
+				for i in range(0, self._train["y"].shape[0]):
+
+					grad_loss_orig = gradient(
+						loss_orig[i], (weights, bias), retain_graph=True
+					)
+					grad_loss_orig = np.array(
+						grad_loss_orig[0][0].tolist() + grad_loss_orig[1].tolist()
+					)
+
+					grad_loss_flip = gradient(
+						loss_flip[i], (weights, bias), retain_graph=True
+					)
+					grad_loss_flip = np.array(
+						grad_loss_flip[0][0].tolist() + grad_loss_flip[1].tolist()
+					)
+
+					grad_diff = grad_loss_flip - grad_loss_orig
+
+					I_theta.append(-np.dot(Hessian_inv, grad_diff).reshape(-1))
+
+				y_pred = model(self._train["X"])
+				orig_loss_fairness = self._DISPLoss(self._train["c"], y_pred)
+				orig_grad_fairness = gradient(
+					orig_loss_fairness, (weights, bias), retain_graph=True
+				)
+				orig_grad_fairness = np.array(
+					orig_grad_fairness[0][0].tolist() + orig_grad_fairness[1].tolist()
+				)
+
+				for i in range(0, self._train["y"].shape[0]):
+					infl[i] = np.dot(orig_grad_fairness, I_theta[i]) * np.exp(
+						-abs(np.dot(orig_grad_utility, I_theta[i]))
+					)
+
 				indices = np.argsort(infl)
 				chosen_F_set = set(chosen_F)
 				actual_indices = []
@@ -185,14 +240,26 @@ class PreProcFlip(object):
 							break
 				chosen_F.extend(actual_indices)
 				for item in actual_indices:
-					self._train['y'][item][0] = 1.0 - self._train['y'][item][0]
-				# END: Pre-processing of fairness
-		
-			if (len(res["test"]["attk"])==0 or res["test"]["attk"][-1] < self._dR) and len(chosen_R) < self._train['X'].shape[0]:
-				# BEGIN: Pre-processing of robustness
-				X_adv = self.AdvExp(X=self._train['X'], y=self._train['y'], method=self._method)
+					self._train["y"][item][0] = 1.0 - self._train["y"][item][0]
+			# END: Pre-processing of fairness
+
+			# BEGIN: Pre-processing of robustness
+			infl = -np.ones(self._train["s"].shape[0])
+			if (
+				len(res[judgement_set]["attk"]) > 0
+				and res[judgement_set]["attk"][-1] < self._dR
+				and len(chosen_R) < self._train["X"].shape[0]
+			):
+				X_adv = self.AdvExp(
+					X=self._train["X"], y=self._train["y"], method=self._method
+				)
 				y_adv_pred = model(X_adv)
-				loss = self._BCELoss(y_adv_pred, self._train['y'], reduction=False).detach().numpy().reshape(-1)
+				loss = (
+					self._BCELoss(y_adv_pred, self._train["y"], reduction=False)
+					.detach()
+					.numpy()
+					.reshape(-1)
+				)
 				indices = np.argsort(loss)
 				chosen_R_set = set(chosen_R)
 				actual_indices = []
@@ -202,21 +269,29 @@ class PreProcFlip(object):
 						if len(actual_indices) == self._k:
 							break
 				chosen_R.extend(actual_indices)
-				if self._train['X_adv'] is None:
-					self._train['X_adv'] = X_adv[actual_indices,:].detach()
+				if self._train["X_adv"] is None:
+					self._train["X_adv"] = X_adv[actual_indices, :].detach()
 				else:
-					self._train['X_adv'] = torch.vstack([
-						self._train['X_adv'],
-						X_adv[actual_indices,:].clone().detach()
-					])
-				# END: Pre-processing of robustness
+					self._train["X_adv"] = torch.vstack(
+						[
+							self._train["X_adv"],
+							X_adv[actual_indices, :].clone().detach(),
+						]
+					)
+			# END: Pre-processing of robustness
 
-			y_pred = model(self._train['X'])
-			metric = Metric(true=self._train_np['y'], pred=y_pred.detach().numpy().reshape(-1))
+			y_pred = model(self._train["X"])
+			metric = Metric(
+				true=self._train_np["y"], pred=y_pred.detach().numpy().reshape(-1)
+			)
 			acc_train_org = metric.accuracy()
-			disp_train_org = metric.positive_disparity(s=self._train_np['s'])
-			y_pred_adv = model(self.AdvExp(X=self._train['X'],y=self._train['y'],method=self._method))
-			acc_train_atk = Metric(true=self._train_np['y'], pred=y_pred_adv.detach().numpy().reshape(-1)).accuracy()
+			disp_train_org = metric.positive_disparity(s=self._train_np["s"])
+			y_pred_adv = model(
+				self.AdvExp(X=self._train["X"], y=self._train["y"], method=self._method)
+			)
+			acc_train_atk = Metric(
+				true=self._train_np["y"], pred=y_pred_adv.detach().numpy().reshape(-1)
+			).accuracy()
 			print(
 				"Iter: %d, Train: (%.4f, %.4f, %.4f)"
 				% (it + 1, acc_train_org, acc_train_atk, disp_train_org),
@@ -229,24 +304,29 @@ class PreProcFlip(object):
 			# BEGIN: Testing
 			if test_output:
 				# X_test.grad=None
-				y_test_pred = model(self._test['X'])
+				y_test_pred = model(self._test["X"])
 				metric = Metric(
-					true=self._test_np['y'], pred=y_test_pred.detach().numpy().reshape(-1)
+					true=self._test_np["y"],
+					pred=y_test_pred.detach().numpy().reshape(-1),
 				)
 				acc_test_org = metric.accuracy()
-				disp_test_org = metric.positive_disparity(s=self._test_np['s'])
+				disp_test_org = metric.positive_disparity(s=self._test_np["s"])
 
-				X_test_adv = self.AdvExp(X=self._test['X'], y=None, method=self._method)
+				X_test_adv = self.AdvExp(X=self._test["X"], y=None, method=self._method)
 				y_test_pred_atk = model(X_test_adv)
 				acc_test_atk = Metric(
-					true=self._test_np['y'], pred=y_test_pred_atk.detach().numpy().reshape(-1)
+					true=self._test_np["y"],
+					pred=y_test_pred_atk.detach().numpy().reshape(-1),
 				).accuracy()
 
 				print(
 					"Test: (%.4f, %.4f, %.4f), Coverage: (R:%.2f, F:%.2f)"
-					% (acc_test_org, acc_test_atk, disp_test_org, 
-						len(chosen_R) / self._train['X'].shape[0],
-						len(chosen_F) / self._train['X'].shape[0],
+					% (
+						acc_test_org,
+						acc_test_atk,
+						disp_test_org,
+						len(chosen_R) / self._train["X"].shape[0],
+						len(chosen_F) / self._train["X"].shape[0],
 					)
 				)
 				res["test"]["orig"].append(acc_test_org)
@@ -256,23 +336,12 @@ class PreProcFlip(object):
 					max_R_value = res["test"]["attk"][-1]
 			# END: Testing
 
-			if res["test"]["attk"][-1] >= self._dR and res["test"]["disp"][-1] <= self._dF:
+			if (
+				res[judgement_set]["attk"][-1] >= self._dR
+				and res[judgement_set]["disp"][-1] <= self._dF
+			):
 				break
 
-			if res["test"]["attk"][-1] < max_R_value - 0.05:
-				res["valid"]=False
-				break
-			if min(infl)>=0 and res["test"]["disp"][-1] > self._dF:
-				res["valid"]=False
-				break
-
-			if self._dR >= res["test"]["orig"][0] + 0.15:
-				res["valid"]=False
-				break
-			if res["test"]["disp"][0] <= self._dF - 0.05:
-				res["valid"]=False
-				break
-			
 		return res
 
 
@@ -300,7 +369,6 @@ def draw(res):
 	m2nd_ax = df_train.plot(secondary_y=True, ax=main_ax)
 	m2nd_ax.set_ylabel("Statistical Parity")
 
-
 	data = np.hstack(
 		[
 			np.array(res["test"]["orig"]).reshape(-1, 1),
@@ -326,7 +394,8 @@ def draw(res):
 if __name__ == "__main__":
 
 	import sys
-	if len(sys.argv)>=2:
+
+	if len(sys.argv) >= 2:
 		data = sys.argv[1]
 		attr = sys.argv[2]
 		method = sys.argv[3]
@@ -334,31 +403,34 @@ if __name__ == "__main__":
 		dR = round(float(sys.argv[5]), 2)
 		k = 0.003
 	else:
-		data = 'adult'
-		attr = 'race'
-		method = 'FGSM'
+		data = "adult"
+		attr = "race"
+		method = "FGSM"
 		dF = 1.0
 		dR = 0.0
 		k = 0.003
 
 	import time
+
 	seed = int(time.time())
 
-	print('Seed is %d'%seed)
+	print("Seed is %d" % seed)
 
-	model = PreProcFlip(data, attr, method=method, k=k, max_iter=1000, seed=seed, dR=dR, dF=dF)
+	model = PreProcFlip(
+		data, attr, method=method, k=k, max_iter=1000, seed=seed, dR=dR, dF=dF
+	)
 	res = model.fit_transform()
 
-	res['data'] = data
-	res['attr'] = attr
-	res['method'] = method
-	res['dF'] = dF
-	res['dR'] = dR
-	res['k'] = k
-	res['seed'] = seed
+	res["data"] = data
+	res["attr"] = attr
+	res["method"] = method
+	res["dF"] = dF
+	res["dR"] = dR
+	res["k"] = k
+	res["seed"] = seed
 
 	import json
 
-	f=open(f'./result/preproc/FnR.txt','a')
-	f.write(json.dumps(res)+'\n')
+	f = open(f"./result/preproc/FnR.txt", "a")
+	f.write(json.dumps(res) + "\n")
 	f.close()
